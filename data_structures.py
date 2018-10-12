083dabb4892555a41569de2ad8cf67da518d150d
unknown <tristan.hehnen@cern.ch>
2018-10-12 21:34:31 +0200

    Created basic-shapes class to create box, plane and point inputs. Linked basic shapes to FDSOBST. Added mentioned code to data_structures as well.

--- data_structures.py
+++ data_structures.py
@@ -85,90 +85,336 @@ class FDSDomain:
         a = 'toast'
 
 
+# #################
+# # BOX SHAPE CLASS
+# class BoxShape:
+#     """
+#     Provide the basic box shape for FDS entities.
+#     """
+#
+#     def __init__(self, box_x1=0.0, box_y1=0.0, box_z1=0.0, box_x2=1.0,
+#                  box_y2=1.0, box_z2=1.0):
+#
+#         self.box_x1 = box_x1
+#         self.box_x2 = box_x2
+#         self.box_y1 = box_y1
+#         self.box_y2 = box_y2
+#         self.box_z1 = box_z1
+#         self.box_z2 = box_z2
+#
+#     def get_box(self):
+#         box = 'XB={},{},{},{},{},{}'.format(self.box_x1, self.box_x2,
+#                                             self.box_y1, self.box_y2,
+#                                             self.box_z1, self.box_z2)
+#         return box
+
+
+# #################
+# # SURFACE CLASS
+# class Surf:
+#     """
+#     Container for surface information of obstructions in FDS.
+#     """
+#     def __init__(self, surf_id='id'):
+#         self.surf_id = surf_id
+#
+#     def get_surf(self):
+#         if self.surf_id is 'id':
+#             surf = "SURF_ID='INERT'"
+#         else:
+#             surf = "SURF_ID='INERT'"
+#         return surf
+
+
+# #################
+# # OBSTRUCTION CLASS
+# class Obst:
+#     """
+#     Set size and position of an obstruction in FDS.
+#     """
+#     def __init__(self, pos_x=0.0, pos_y=0.0, pos_z=0.0,
+#                  len_x=1.0, len_y=1.0, len_z=1.0):
+#
+#         # Starting point.
+#         self.pos_x = float(pos_x)
+#         self.pos_y = float(pos_y)
+#         self.pos_z = float(pos_z)
+#
+#         # Edge length.
+#         self.len_x = float(len_x)
+#         self.len_y = float(len_y)
+#         self.len_z = float(len_z)
+#
+#     def get_obst(self):
+#         # Calculate dimensions and position of the obstruction.
+#         # Start.
+#         x1 = self.pos_x
+#         y1 = self.pos_y
+#         z1 = self.pos_z
+#
+#         # End.
+#         x2 = self.pos_x + self.len_x
+#         y2 = self.pos_y + self.len_y
+#         z2 = self.pos_z + self.len_z
+#
+#         # Create box shape of obstruction.
+#         bx1 = BoxShape(x1, x2,
+#                        y1, y2,
+#                        z1,  z2)
+#
+#         # Attach surface to obstruction.
+#         sf1 = Surf()
+#
+#         # Finalise obstruction for FDS input file.
+#         obst = "&OBST {}, {} /".format(bx1.get_box(), sf1.get_surf())
+#
+#         return obst
+
+
 #################
-# BOX SHAPE CLASS
-class BoxShape:
+# Basic shapes
+class FDSBasicShapes:
     """
-    Provide the basic box shape for FDS entities.
+    Provide the basic shapes for FDS entities - boxes, planes and points.
     """
+    basic_shape_temp = {'xb': "XB = {}, {}, {}, {}, {}, {}",
+                        'pb': "PB{} = {}",
+                        'xyz': "XYZ = {}"}
 
-    def __init__(self, box_x1=0.0, box_y1=0.0, box_z1=0.0, box_x2=1.0,
-                 box_y2=1.0, box_z2=1.0):
+    def __init__(self, x=0.0, y=0.0, z=0.0,
+                 len_x=1.0, len_y=1.0, len_z=1.0,
+                 loc_x=0.0, loc_y=0.0, loc_z=0.0,
+                 digits=2, lead_chars=5):
+        """
 
-        self.box_x1 = box_x1
-        self.box_x2 = box_x2
-        self.box_y1 = box_y1
-        self.box_y2 = box_y2
-        self.box_z1 = box_z1
-        self.box_z2 = box_z2
+        :param x:
+        :param y:
+        :param z:
+        :param len_x:
+        :param len_y:
+        :param len_z:
+        :param loc_x:
+        :param loc_y:
+        :param loc_z:
+        :param digits: Number of characters added to total characters to account
+            for a desired amount of digits after the decimal point.
+        :param lead_chars: Number of characters before the decimal point,
+            including the sign (-) and the decimal point. Assuming a few hundred
+            meters are sufficient for FDS, this leads to a default of 5 (-999.).
+        """
 
-    def get_box(self):
-        box = 'XB={},{},{},{},{},{}'.format(self.box_x1, self.box_x2,
-                                            self.box_y1, self.box_y2,
-                                            self.box_z1, self.box_z2)
-        return box
+        self.x = x
+        self.y = y
+        self.z = z
+        self.len_x = len_x
+        self.len_y = len_y
+        self.len_z = len_z
+        self.loc_x = loc_x
+        self.loc_y = loc_y
+        self.loc_z = loc_z
+        self.digits = digits
+        self.lead_chars = lead_chars
 
+    def compile_box(self, show=False):
+        # Set number digits after decimal point.
+        digit_temp = "{" + ":.{}f".format(self.digits) + "}"
 
-#################
-# SURFACE CLASS
-class Surf:
-    """
-    Container for surface information of obstructions in FDS.
-    """
-    def __init__(self, surf_id='id'):
-        self.surf_id = surf_id
+        # Set number of total characters, without decimal point, thus
+        # defining leading spaces.
+        total_chars_temp = "{" + ":>{}s".format(
+            self.lead_chars + self.digits) + "}"
 
-    def get_surf(self):
-        if self.surf_id is 'id':
-            surf = "SURF_ID='INERT'"
-        else:
-            surf = "SURF_ID='INERT'"
-        return surf
+        # Define box size and location.
+        x1 = self.x + self.loc_x
+        y1 = self.y + self.loc_y
+        z1 = self.z + self.loc_z
+        x2 = self.len_x + self.loc_x
+        y2 = self.len_y + self.loc_y
+        z2 = self.len_z + self.loc_z
+        coords = [x1, y1, z1, x2, y2, z2]
+
+        #
+        nl = []
+        for coord in coords:
+            new_digit = digit_temp.format(coord)
+            new_space = total_chars_temp.format(new_digit)
+            nl.append(new_space)
+
+        box_shape = self.basic_shape_temp['xb']
+        shape_line = box_shape.format(nl[0],
+                                      nl[1],
+                                      nl[2],
+                                      nl[3],
+                                      nl[4],
+                                      nl[5])
+
+        if show is True:
+            # for line in shape_lines:
+            print(shape_line)
+            print("Leading chars: ", self.lead_chars)
+            print("Digits: ", digit_temp)
+            print("Total chars: ", total_chars_temp)
+
+        return shape_line
+
+    def compile_plane(self, show=False):
+        plane_line = 'Plane is work in progress'
+        return plane_line
+
+    def compile_point(self, show=False):
+        point_line = 'Point is work in progress'
+        return point_line
 
 
 #################
 # OBSTRUCTION CLASS
-class Obst:
-    """
-    Set size and position of an obstruction in FDS.
-    """
-    def __init__(self, pos_x=0.0, pos_y=0.0, pos_z=0.0,
-                 len_x=1.0, len_y=1.0, len_z=1.0):
+class FDSOBST:
+    obst_temp = {'init': "&OBST ID = '{}'",
+                 'allow_vent': "ALLOW_VENT = .{}.",
+                 'bndf_face': "BNDF_FACE({}) = .{}.",
+                 'bndf_obst': "BNDF_OBST = .{}.",
+                 'bulk_density': "BULK_DENSITY = {}",
+                 'color': "COLOR = {}",
+                 'ctrl_id': "CTRL_ID = {}",
+                 'devc_id': "DEVC_ID = {}",
+                 'evacuation': "EVACUATION = .{}.",
+                 'ht3d': "HT3D = .{}.",
+                 'mesh_id': "MESH_ID = {}",
+                 'mult_id': "MULT_ID = {}",
+                 'outline': "OUTLINE = .{}.",
+                 'overlay': "OVERLAY = .{}.",
+                 'permit_hole': "PERMIT_HOLE = .{}.",
+                 'prop_id': "PROP_ID = {}",
+                 'removable': "REMOVABLE = .{}.",
+                 'rgb': "RGB(3) = {}",
+                 'surf_id': "SURF_ID = {}",
+                 'surf_id6': "SURF_ID6(6) = {}",
+                 'surf_ids': "SURF_IDS(3) = {}",
+                 'texture_origin': "TEXTURE_ORIGIN(3) = {}",
+                 'thicken': "THICKEN = .{}.",
+                 'transparency': "TRANSPARENCY = {}",
+                 'xb': "XB(6) = {}"}
 
-        # Starting point.
-        self.pos_x = float(pos_x)
-        self.pos_y = float(pos_y)
-        self.pos_z = float(pos_z)
+    def __init__(self, init, allow_vent=None, bndf_face=None,
+                 bndf_obst=None, bulk_density=None, color=None,
+                 ctrl_id=None, devc_id=None, evacuation=None,
+                 ht3d=None, mesh_id=None, mult_id=None,
+                 outline=None, overlay=None, permit_hole=None,
+                 prop_id=None, removable=None, rgb=None,
+                 surf_id=None, surf_id6=None, surf_ids=None,
+                 texture_origin=None, thicken=None,
+                 transparency=None, xb=None,
+                 x=0.0, y=0.0, z=0.0,
+                 len_x=1.0, len_y=1.0, len_z=1.0,
+                 loc_x=0.0, loc_y=0.0, loc_z=0.0, ):
 
-        # Edge length.
-        self.len_x = float(len_x)
-        self.len_y = float(len_y)
-        self.len_z = float(len_z)
+        self.init = init
+        self.allow_vent = allow_vent
+        self.bndf_face = bndf_face
+        self.bndf_obst = bndf_obst
+        self.bulk_density = bulk_density
+        self.color = color
+        self.ctrl_id = ctrl_id
+        self.devc_id = devc_id
+        self.evacuation = evacuation
+        self.ht3d = ht3d
+        self.mesh_id = mesh_id
+        self.mult_id = mult_id
+        self.outline = outline
+        self.overlay = overlay
+        self.permit_hole = permit_hole
+        self.prop_id = prop_id
+        self.removable = removable
+        self.rgb = rgb
+        self.surf_id = surf_id
+        self.surf_id6 = surf_id6
+        self.surf_ids = surf_ids
+        self.texture_origin = texture_origin
+        self.thicken = thicken
+        self.transparency = transparency
+        self.xb = xb
 
-    def get_obst(self):
-        # Calculate dimensions and position of the obstruction.
-        # Start.
-        x1 = self.pos_x
-        y1 = self.pos_y
-        z1 = self.pos_z
+        self.x = x
+        self.y = y
+        self.z = z
+        self.len_x = len_x
+        self.len_y = len_y
+        self.len_z = len_z
+        self.loc_x = loc_x
+        self.loc_y = loc_y
+        self.loc_z = loc_z
 
-        # End.
-        x2 = self.pos_x + self.len_x
-        y2 = self.pos_y + self.len_y
-        z2 = self.pos_z + self.len_z
+        self.param_base = [['init', self.init],
+                           ['allow_vent', allow_vent],
+                           ['bndf_face', self.bndf_face],
+                           ['bndf_obst', self.bndf_obst],
+                           ['bulk_density', self.bulk_density],
+                           ['color', self.color],
+                           ['ctrl_id', self.ctrl_id],
+                           ['devc_id', self.devc_id],
+                           ['evacuation', self.evacuation],
+                           ['ht3d', self.ht3d],
+                           ['mesh_id', self.mesh_id],
+                           ['mult_id', self.mult_id],
+                           ['outline', self.outline],
+                           ['overlay', self.overlay],
+                           ['permit_hole', self.permit_hole],
+                           ['prop_id', self.prop_id],
+                           ['removable', self.removable],
+                           ['rgb', self.rgb],
+                           ['surf_id', self.surf_id],
+                           ['surf_id6', self.surf_id6],
+                           ['surf_ids', self.surf_ids],
+                           ['texture_origin', self.texture_origin],
+                           ['thicken', self.thicken],
+                           ['transparency', self.transparency],
+                           ['xb', self.xb]]
 
-        # Create box shape of obstruction.
-        bx1 = BoxShape(x1, x2,
-                       y1, y2,
-                       z1,  z2)
+        self.box = [['x', self.x],
+                    ['y', self.y],
+                    ['z', self.z],
+                    ['len_x', self.len_x],
+                    ['len_y', self.len_y],
+                    ['len_z', self.len_z],
+                    ['loc_x', self.loc_x],
+                    ['loc_y', self.loc_y],
+                    ['loc_z', self.loc_z]]
 
-        # Attach surface to obstruction.
-        sf1 = Surf()
+    def compile_obst(self, show=False):
+        obst_lines = []
 
-        # Finalise obstruction for FDS input file.
-        obst = "&OBST {}, {} /".format(bx1.get_box(), sf1.get_surf())
+        # Initialise the obstruction.
+        par = self.param_base[0]
+        new_par = self.obst_temp[par[0]].format(par[1])
+        obst_lines.append(new_par + ',')
 
-        return obst
+        # Define size and location of the obstruction.
+        obst_dim = FDSBasicShapes(self.x,
+                                  self.x,
+                                  self.x,
+                                  self.len_x,
+                                  self.len_y,
+                                  self.len_z,
+                                  self.loc_x,
+                                  self.loc_y,
+                                  self.loc_z)
+        new_par = obst_dim.compile_box()
+        obst_lines.append('      ' + new_par + ',')
+
+        # Iterate over the remaining parameters of the
+        # obstruction class.
+        for par in self.param_base[1:]:
+            if par[1] is not None:
+                new_par = self.obst_temp[par[0]].format(par[1])
+                obst_lines.append('      ' + new_par + ',')
+
+        obst_lines[-1] = obst_lines[-1][:-1] + ' /\n'
+
+        if show is True:
+            for line in obst_lines:
+                print(line)
+
+        return obst_lines
 
 
 #################
